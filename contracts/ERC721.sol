344b95db88b9bc1111fa7a5103f7369ee38e379d
Qadir Adesoye <abdqadirola@gmail.com>
2025-02-11 04:47:01 +0100

    update

--- contracts/ERC721.sol
+++ contracts/ERC721.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.22;
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
+
 contract Tickets is ERC721, Ownable {
     uint256 private _nextTokenId;
 
